bcb6a700db0ad84af245aba7ac579af341c6a253
KukiKings <91531741+KukiKings@users.noreply.github.com>
2025-07-29 00:22:12 +1000

    referral.js xp.js next.config.js

--- xp.js
+++ xp.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "XP and quest engine activated" });
+  res.status(200).json({ message: "XP quest engine activated" });
 }
