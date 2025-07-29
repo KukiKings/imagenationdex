bdde92807e405de9f0dea3a167db8a87ccaacdd2
KukiKings <91531741+KukiKings@users.noreply.github.com>
2025-07-29 13:11:48 +1000

    Create ConnectWallet.jsx
    Add ConnectWallet button”

---
+++ components/ConnectWallet.jsx
@@ -0,0 +1,8 @@
+// components/ConnectWallet.jsx
+import { WalletMultiButton }
+  from '@solana/wallet-adapter-react-ui';
+
+export default function ConnectWallet({ className }) {
+  return <WalletMultiButton className={className} />;
+}
+Add ConnectWallet button”
